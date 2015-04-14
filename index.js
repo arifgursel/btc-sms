de38c122c49acf9de5a3f494ddc147301575b27b
Randall Degges <r@rdegges.com>
2015-04-14 16:25:51 -0700

    Initializing static asset handling.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.set('view engine', 'jade');
 app.set('views', './src/views');
 
 // Middlewares
+app.use('/static', express.static('./bower_components', {
+  index: false,
+  redirect: false
+}));
 app.use(stormpath.init(app));
 
 // Server
