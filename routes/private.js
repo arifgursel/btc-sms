b6ef1252023d4b8c92af96e2e3bcd7095372e792
Randall Degges <r@rdegges.com>
2015-04-16 16:17:38 -0700

    Removing an unnecessary middleware.

--- routes/private.js
+++ routes/private.js
@@ -6,20 +6,6 @@ var stormpath = require('express-stormpath');
 // Globals
 var router = express.Router();
 
-// Middlewares
-router.use(function(req, res, next) {
-  var apiKeys = [];
-
-  req.user.apiKeys.each(function(key, cb) {
-    apiKeys.push(key);
-    cb();
-  }, function(err) {
-    if (err) return next(err);
-    res.locals.apiKeys = apiKeys;
-    next();
-  });
-});
-
 // Routes
 router.get('/', function(req, res) {
   res.render('dashboard');
