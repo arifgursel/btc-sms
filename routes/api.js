5a9509bf95b3f34576e426f83c8c61a41b0a3942
Randall Degges <r@rdegges.com>
2015-04-17 20:46:10 -0700

    Adding in accounting.

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,8 @@ router.post('/message', stormpath.apiAuthenticationRequired, function(req, res) 
     return res.status(400).json({ error: 'phoneNumber is required.' });
   } else if (!BTC_EXCHANGE_RATE) {
     return res.status(500).json({ error: "We're having trouble getting the exchange rates right now. Try again soon!" });
+  } else if (req.user.customData.balance < COST_PER_QUERY) {
+    return res.status(402).json({ error: 'Payment required. You need to deposit funds into your account.' });
   }
 
   var message = '1 Bitcoin is currently worth $' + BTC_EXCHANGE_RATE  + ' USD.';
@@ -32,6 +34,7 @@ router.post('/message', stormpath.apiAuthenticationRequired, function(req, res) 
     if (err) return res.status(500).json({ error: "We couldn't send the SMS message. Try again soon!" });
 
     req.user.customData.balance -= COST_PER_QUERY;
+    req.user.customData.totalQueries += 1;
     req.user.customData.save();
 
     res.json({ phoneNumber: req.body.phoneNumber, message: message, cost: COST_PER_QUERY });
